c107d849860e98294bfba300822bb58b68ef050b
bananin26 <mabeblue26@gmail.com>
2023-09-05 22:11:26 -0500

    :))

---
+++ takemehometf/src/main/java/pe/edu/upc/aww/takemehometf/security/JwtRequestFilter.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.aww.takemehometf.security;
+
+public class JwtRequestFilter {
+}
---
+++ takemehometf/src/main/java/pe/edu/upc/aww/takemehometf/serviceimplements/JwUserDetailsService.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.aww.takemehometf.serviceimplements;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwUserDetailsService {
+
+
+
+}
