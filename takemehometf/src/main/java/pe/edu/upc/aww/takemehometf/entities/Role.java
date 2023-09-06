1993ba63888adb557276f61996416c2fa16f13a1
bananin26 <mabeblue26@gmail.com>
2023-09-05 21:15:30 -0500

    :0

---
+++ takemehometf/src/main/java/pe/edu/upc/aww/takemehometf/entities/Role.java
@@ -0,0 +1,45 @@
+package pe.edu.upc.aww.takemehometf.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="roles", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","rol"})})
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String rol;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
