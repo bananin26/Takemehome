9cd3f22030003a128e9debb2c709242e6c5af374
bananin26 <mabeblue26@gmail.com>
2023-09-06 00:38:49 -0500

    :))

--- takemehometf/src/main/java/pe/edu/upc/aww/takemehometf/security/JwtTokenUtil.java
+++ takemehometf/src/main/java/pe/edu/upc/aww/takemehometf/security/JwtTokenUtil.java
@@ -49,7 +49,7 @@ public class JwtTokenUtil implements Serializable {
     //generate token for user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        claims.put("nombre", "mabe");
+        claims.put("nombre", "rosa");
         claims.put("role",userDetails.getAuthorities().stream().map(r->r.getAuthority()).collect(Collectors.joining()));
         return doGenerateToken(claims, userDetails.getUsername());
     }
